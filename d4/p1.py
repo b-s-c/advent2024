d085938e1ccecb1df5b7885405b81a6be113bb4a
brandon <44364296+b-s-c@users.noreply.github.com>
2024-12-04 16:03:11 +0000

    make d4p1 more readable (but keep the logic)

--- d4/p1.py
+++ d4/p1.py
@@ -2,29 +2,22 @@ grid = [(line.strip()) for line in open('real.input')]
 grid_t = ["".join(line) for line in zip(*grid, strict=True)]
 grid_r = ["".join(reversed(line)) for line in grid]
 
-grid_d1 = []
-for i in range(0, len(grid)):
-    grid_d1.append((" "*i + grid[i])[:len(grid)])
-grid_d1 = ["".join(line) for line in zip(*grid_d1, strict=True)]
-temp = []
-for i in range(0, len(grid)):
-    temp.append(grid[i][:i].rjust(len(grid))) #[i:])
-temp = ["".join(line) for line in zip(*temp, strict=True)]
-grid_d1.extend(temp)
-
-grid_d2 = []
-for i in range(0, len(grid_r)):
-    grid_d2.append((" "*i + grid_r[i])[:len(grid_t)])
-grid_d2 = ["".join(line) for line in zip(*grid_d2, strict=True)]
-temp = []
-for i in range(0, len(grid_r)):
-    temp.append(grid_r[i][:i].rjust(len(grid_t))) #[i:])
-temp = ["".join(line) for line in zip(*temp, strict=True)]
-grid_d2.extend(temp)
+def get_diagonals(grid) -> list[str]:
+    temp_1 = []
+    for i in range(0, len(grid)):
+        temp_1.append((" "*i + grid[i])[:len(grid)])
+    temp_1 = ["".join(line) for line in zip(*temp_1, strict=True)]
+    temp_2 = []
+    for i in range(0, len(grid)):
+        temp_2.append(grid[i][:i].rjust(len(grid))) #[i:])
+    temp_2 = ["".join(line) for line in zip(*temp_2, strict=True)]
+    temp_1.extend(temp_2)
+    return temp_1
 
 print(len(grid), len(grid[0]))
 
 count = 0
+
 for line in grid:
     count += line.count("XMAS")
     count += line.count("SAMX")
@@ -35,12 +28,12 @@ for line in grid_t:
     count += line.count("SAMX")
     print(line, count)
 print(count)
-for line in grid_d1:
+for line in get_diagonals(grid):
     count += line.count("XMAS")
     count += line.count("SAMX")
     print(line, count)
 print(count)
-for line in grid_d2:
+for line in get_diagonals(grid_r):
     count += line.count("XMAS")
     count += line.count("SAMX")
     print(line, count)
